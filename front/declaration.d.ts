30de98bcddb356a733d5a6ac23f50a78f935da85
sarakaros <socthanhnhi@gmail.com>
2025-11-30 23:17:06 +0700

    Add declare .css

--- front/declaration.d.ts
+++ front/declaration.d.ts
@@ -2,3 +2,5 @@ declare module 'swiper/css';
 declare module 'swiper/css/navigation';
 declare module 'swiper/css/pagination';
 declare module 'swiper/css/scrollbar';
+declare module "leaflet/dist/leaflet.css";
+declare module "*.css";
