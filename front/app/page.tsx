cd171cd7708ff8a1f41da6f958cb3255d188bdaa
Altan0120 <tranthanhtan509@gmail.com>
2025-12-01 16:24:37 +0700

    change id section calling

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,21 +1,18 @@
-
-import React from 'react'
+import React from "react";
 import HeroSection from "@/components/sections/hero-section";
 import LocationsSection from "@/components/sections/locations-section";
 import GuideSection from "@/components/sections/guides-section";
 import ChatSection from "@/components/sections/chat-section";
 import MapSection from "@/components/sections/map-section";
 
-
 export default function Home() {
   return (
-  <main>
-    <HeroSection/>
-    <LocationsSection/>
-    <GuideSection />
-    <MapSection/>
-    <ChatSection/>
-  </main>
+    <main>
+      <HeroSection />
+      <LocationsSection />
+      <GuideSection />
+      <MapSection />
+      <ChatSection />
+    </main>
   );
 }
-
