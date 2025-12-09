3b63e4b231be601722a32fbe94ac0d445c9918ae
phgthao2204~ <phuongthao04xx@gmail.com~>
2025-12-10 01:47:40 +0700

    update locations card

--- client/components/features/locations-card.tsx
+++ client/components/features/locations-card.tsx
@@ -1,28 +1,37 @@
+"use client";
+
 import { Locations } from "@/lib/types";
 import Image from "next/image";
 import { FaLocationArrow } from "react-icons/fa";
+import { useRouter } from "next/navigation";
 
-export default function LocationsCard({
-  Locations,
-}: {
-  Locations: Locations;
-}) {
+interface Props {
+  location: Locations;
+}
+
+export default function LocationsCard({ location }: Props) {
+  const router = useRouter();
+
   return (
-    <div className="cursor-pointer shadow-xl hover:shadow-2xl rounded-md overflow-hidden relative h-[457px]">
+    <div
+      className="cursor-pointer shadow-xl hover:shadow-2xl rounded-md overflow-hidden relative h-[457px]"
+      onClick={() => router.push(`/locations/${location.id}`)}
+    >
       <Image
         className="object-cover z-3"
         fill
-        src={Locations.image}
-        alt={""}
+        src={location.image}
+        alt={location.location}
       />
+
       <div className="absolute bottom-0 left-0 right-0 z-10 px-6 pt-4 pb-10 text-dark bg-white">
         <h5 className="flex justify-between font-poppins text-lg items-center">
-          {Locations.location}
+          {location.location}
         </h5>
 
         <p className="mt-5 flex gap-2 items-center capitalize text-lg">
           <FaLocationArrow className="text-black" />
-          {Locations.duration}
+          {location.duration}
         </p>
       </div>
     </div>
