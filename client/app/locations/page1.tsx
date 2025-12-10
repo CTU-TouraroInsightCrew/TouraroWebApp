54ecfe37595a5c33e3e4b49d5ed5a3d10d4cb0ca
phgthao2204~ <phuongthao04xx@gmail.com~>
2025-12-10 17:16:17 +0700

    show details location

---
+++ client/app/locations/page1.tsx
@@ -0,0 +1,14 @@
+/*
+ * This file is part of TouraroWebApp.
+ * Licensed under the GPL-3.0-only License.
+ * Copyright (c) 2025 CTU-TouraroInsigtCrew
+ */
+
+import LocationSection from "@/components/sections/locations-section";
+
+
+export default function LocationPage() {
+    return (
+                <LocationSection />
+    );
+}
